67a6a56896f3ff4eb36a998964a30e896841072f
rkoczur <kocz@kocz.hu>
2020-05-24 19:56:10 +0200

    Made the appropriate functions as described in the assesment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix and cacheSolve functions in a pair allow to compute the inverse of a matrix 
+## loaded into makeCacheMatrix. However if the inverse is alrady computed, it is stored in
+## the enviroment of the makeCacheMatrix together with the original matrix. 
 
-## Write a short comment describing this function
+## the makeCacheMatrix function takes a matrix as an argument, and store it together with
+## setter and getter functions. The inverse variable stores the inverse of the matrix in 
+## the same object. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inverse <- NULL
+        set <- function(sourceMatrix) {
+                x <<- sourceMatrix
+                inverse <<- NULL
+        }
+        get <- function() x
+        makeInverse <- function() {
+                inverse <<- solve(x)
+        }
+        getInverse <- function() inverse
+        list(set = set, get = get,
+             makeInverse = makeInverse,
+             getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## If you feed an object created with the makeCachematrix to cacheSolve, it returns with
+## the inverse of the original matrix. But if it is already computed, it get the data with
+## getInverse function, so it no need to compute again.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inverse <- x$getInverse()
+        if(!is.null(inverse)) {
+                print("getting inverse from cache")
+                return(inverse)
+        }
+        inverse <- x$makeInverse()
+        inverse
 }
